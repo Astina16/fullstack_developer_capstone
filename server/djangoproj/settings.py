5c759f34425bdb8fe42b03d70f258ddae88de94c
Astina16 <astina.d@somaiya.edu>
2025-11-30 23:50:26 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "fullstack-developer-capstone-1-38l0.onrender.com",
+]
+
 
 CSRF_TRUSTED_ORIGINS = []
 

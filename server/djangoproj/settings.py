6b40e1025bcff7d74be1505630369c8c30799a1b
Astina16 <astina.d@somaiya.edu>
2025-11-30 23:08:43 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -43,7 +43,7 @@ REST_FRAMEWORK = {
 # Application definition
 
 INSTALLED_APPS = [
-    'server.djangoapp.apps.DjangoappConfig',
+    'djangoapp.apps.DjangoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
